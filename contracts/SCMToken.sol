972517873765ffd36dc0319df8b633cc493b760a
czhc <cedriczhchan@gmail.com>
2021-06-28 23:28:39 +0800

    Update Token migraiton

--- contracts/SCMToken.sol
+++ contracts/SCMToken.sol
@@ -4,7 +4,7 @@ pragma solidity >= 0.8.6 < 0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SCMToken is ERC20 {
-    constructor (string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 100 * 10 ** uint(decimals()));
+    constructor (string memory name, string memory symbol, uint256 _supply) ERC20(name, symbol) {
+        _mint(msg.sender, _supply * 10 ** uint(decimals()));
     }
 }

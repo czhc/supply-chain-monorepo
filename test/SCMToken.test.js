8f5e3eb5060324f66a0518a778fcfacca06d8f0e
czhc <cedriczhchan@gmail.com>
2021-06-29 00:04:41 +0800

    Refactor chai

--- test/SCMToken.test.js
+++ test/SCMToken.test.js
@@ -1,14 +1,7 @@
 const SCMToken = artifacts.require('SCMToken');
 
-var chai = require('chai');
+const chai = require('./chaisetup.js');
 const BN = web3.utils.BN;
-
-const chaiBN = require('chai-bn')(BN);
-chai.use(chaiBN);
-
-var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-
 const expect = chai.expect;
 
 contract( "SCM Token", function(accounts) {
@@ -29,18 +22,18 @@ contract( "SCM Token", function(accounts) {
     let instance = this.scmToken;
     let totalSupply = await instance.totalSupply();
 
-    await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
-    await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
-    await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
-    await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+    return expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+    return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+    return expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
   })
 
   it("not possible to send more tokens than balance", async()=>{
     let instance = this.scmToken;
     let balanceOfAccount = await instance.balanceOf(initialHolder);
-    await expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
+    return expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
 
-    await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
+    return  expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
   })
 
 });

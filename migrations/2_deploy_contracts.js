9fc4dacb8efef130871dd8a2c98a981db3585ea0
czhc <cedriczhchan@gmail.com>
2021-06-28 14:39:00 +0800

    Create SCM token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,9 @@
 var ItemManager = artifacts.require("./ItemManager.sol");
-module.exports = function(deployer) {
-  deployer.deploy(ItemManager);
+var [name, token] = ['SCT Token', 'SCT'];
+var SCToken = artifacts.require('./SCMToken.sol');
+
+module.exports = async function(deployer) {
+  console.log('name', name);
+  await deployer.deploy(ItemManager);
+  await deployer.deploy(SCToken, name, token);
 };
